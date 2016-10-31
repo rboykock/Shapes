e3bfbf4bfd96998e5522c9fedb624cd316f19de9
rboyko <you@example.com>
2016-10-31 13:31:41 +0200

    Initial commit

---
+++ src/pro/cherkassy/rboyko/interfaces/IInputVaries.java
@@ -0,0 +1,10 @@
+package pro.cherkassy.rboyko.interfaces;
+
+import java.io.BufferedReader;
+
+/**
+ * Created by rboyko on 31.10.16.
+ */
+public interface IInputVaries {
+    public double[] getParams( BufferedReader br);
+}
---
+++ src/pro/cherkassy/rboyko/interfaces/IShape.java
@@ -0,0 +1,11 @@
+package pro.cherkassy.rboyko.interfaces;
+
+import pro.cherkassy.rboyko.Shapes;
+
+/**
+ * Created by rboyko on 28.10.16.
+ */
+public interface IShape {
+    public double calculateArea();
+    public double calculatePerimeter();
+}
